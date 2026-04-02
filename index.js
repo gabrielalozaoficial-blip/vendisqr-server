b1a4ff23048c4c2558c7a9795b99bde3bd8d1695
gabrielalozaoficial-blip <gabrielalozaoficial@gmail.com>
2026-04-02 15:46:34 -0400

    Refactor server to use dynamic PORT variable

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const express = require("express");
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Servidor funcionando 🚀");
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server running");
 });
